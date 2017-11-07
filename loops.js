85af09067f1e183fd7182a436f063fbe4116a6c7
Oleg Chursin <olegchursin@gmail.com>
2017-11-07 19:49:44 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -19,12 +19,12 @@ function whileLoop(n) {
   return "done";
 }
 
-
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
 
 function doWhileLoop(array) {
-  function maybeTrue() {
-    return Math.random() >= 0.5
-  }
+  
   do {
     array.pop()
   } while array.length > 0 && maybeTrue();
